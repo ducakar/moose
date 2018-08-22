2851cda43b95043179e2baff2567677cf39e4f2e
Davorin Učakar <davorin.ucakar@gmail.com>
2018-08-22 23:34:45 +0200

    Generate spearate nice & offensive fortune lists
    - add Library.AppendDir (moved from genfortunes/main.go)
    - fortunes/fortunes.go now contains sub-splices NiceFortunes and
      OffensiveFortunes

--- library.go
+++ library.go
@@ -12,6 +12,17 @@ type Library struct {
 	Fortunes []string
 }
 
+// Get a random fortune.
+func (l *Library) Get() string {
+	nFortunes := len(l.Fortunes)
+	if nFortunes == 0 {
+		return ""
+	}
+
+	index := rand.Intn(len(l.Fortunes))
+	return l.Fortunes[index]
+}
+
 // Append fortunes from a given file to the library.
 func (l *Library) Append(path string, offensive bool) error {
 	file, err := os.Open(path)
@@ -24,35 +35,36 @@ func (l *Library) Append(path string, offensive bool) error {
 	if err != nil {
 		return err
 	}
-
 	if offensive {
 		data = decode(data)
 	}
-
 	// Every fortune ends in "\n%\n", so we gen an empty entry at the end.
 	fortunes := strings.Split(string(data), "%\n")
 	if len(fortunes) != 0 {
 		fortunes = fortunes[:len(fortunes)-1]
 	}
-
 	l.Fortunes = append(l.Fortunes, fortunes...)
 	return nil
 }
 
-// Get a random fortune.
-func (l *Library) Get() string {
-	nFortunes := len(l.Fortunes)
-	if nFortunes == 0 {
-		return ""
+// AppendDir appends fortunes from files in a given directory that do not end
+// with `*.dat`. Does not recourse.
+func (l *Library) AppendDir(dirPath string, offensive bool) error {
+	files, err := ioutil.ReadDir(dirPath)
+	if err != nil {
+		return err
 	}
 
-	index := rand.Intn(len(l.Fortunes))
-	return l.Fortunes[index]
-}
-
-// List all fortunes from the library.
-func (l *Library) List() []string {
-	return l.Fortunes
+	for _, f := range files {
+		if f.IsDir() || strings.HasSuffix(f.Name(), ".dat") {
+			continue
+		}
+		path := dirPath + string(os.PathSeparator) + f.Name()
+		if err := l.Append(path, offensive); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func decode(data []byte) []byte {
